8973e58a5e43b8c6799bead3d8e4481c4b821e52
RyanHazlewood0 <114792045+RyanHazlewood0@users.noreply.github.com>
2023-09-18 11:20:01 +1200

    complete

--- script.js
+++ script.js
@@ -55,10 +55,14 @@ function placeMark() {
         cell[i].addEventListener('click', function() {
             if (cell[i].innerText === '') 
                 cell[i].innerText = activePlayer.mark
-            checkWinX()
-            CheckWinO()
-            checkDraw()
-                switchPlayer()
+            
+                setTimeout(() => {
+                checkWinX()
+                checkWinO()
+                checkDraw()
+                    switchPlayer()
+            }, 0)
+               
         })
     }
 }
@@ -75,50 +79,51 @@ let cellSeven = document.getElementById('cell7')
 let cellEight = document.getElementById('cell8')
 let cellNine = document.getElementById('cell9')
 
-function checkWinX() {
-if (cellOne.textContent === 'X' && cellFour.textContent === 'X' && cellSeven.textContent === 'X') {
-    alert('Player X Won!')
-   } else if (cellOne.textContent === 'X' && cellTwo.textContent === 'X' && cellThree.textContent === 'X') {
-    alert('Player X Won!')
-   } else if (cellFour.textContent === 'X' && cellFive.textContent === 'X' && cellSix.textContent === 'X') {
-    alert('Player X Won!')
-}  else if (cellSeven.textContent === 'X' && cellEight.textContent === 'X' && cellNine.textContent === 'X') {
-    alert('Player X Won!')
-} else if (cellThree.textContent === 'X' && cellSix.textContent === 'X' && cellNine.textContent === 'X') {
-    alert('Player X Won!')
-} else if (cellTwo.textContent === 'X' && cellFive.textContent === 'X' && cellEight.textContent === 'X') {
-    alert('Player X Won!')
-} else if (cellOne.textContent === 'X' && cellFive.textContent === 'X' && cellNine.textContent === 'X') {
-    alert('Player X Won!')
-} else if (cellThree.textContent === 'X' && cellFive.textContent === 'X' && cellSeven.textContent === 'X') {
-    alert('Player X Won!')
-}
-
-}
-
-function CheckWinO() {
-    if (cellOne.textContent === 'O' && cellFour.textContent === 'O' && cellSeven.textContent === 'O') {
-        alert('Player O Won!')
-       } else if (cellOne.textContent === 'O' && cellTwo.textContent === 'O' && cellThree.textContent === 'O') {
-        alert('Player O Won!')
-       } else if (cellFour.textContent === 'O' && cellFive.textContent === 'O' && cellSix.textContent === 'O') {
-        alert('Player O Won!')
-    }  else if (cellSeven.textContent === 'O' && cellEight.textContent === 'O' && cellNine.textContent === 'O') {
-        alert('Player O Won!')
-    } else if (cellThree.textContent === 'O' && cellSix.textContent === 'O' && cellNine.textContent === 'O') {
-        alert('Player O Won!')
-    } else if (cellTwo.textContent === 'O' && cellFive.textContent === 'O' && cellEight.textContent === 'O') {
-        alert('Player O Won!')
-    } else if (cellOne.textContent === 'O' && cellFive.textContent === 'O' && cellNine.textContent === 'O') {
-        alert('Player O Won!')
-    } else if (cellThree.textContent === 'O' && cellFive.textContent === 'O' && cellSeven.textContent === 'O') {
-        alert('Player O Won!')
+function checkWinX(showAlert = true) {
+    if (
+       (cellOne.textContent === 'X' && cellFour.textContent === 'X' && cellSeven.textContent === 'X') ||
+       (cellOne.textContent === 'X' && cellTwo.textContent === 'X' && cellThree.textContent === 'X') ||
+       (cellFour.textContent === 'X' && cellFive.textContent === 'X' && cellSix.textContent === 'X') ||
+       (cellSeven.textContent === 'X' && cellEight.textContent === 'X' && cellNine.textContent === 'X') ||
+       (cellThree.textContent === 'X' && cellSix.textContent === 'X' && cellNine.textContent === 'X') ||
+       (cellTwo.textContent === 'X' && cellFive.textContent === 'X' && cellEight.textContent === 'X') ||
+       (cellOne.textContent === 'X' && cellFive.textContent === 'X' && cellNine.textContent === 'X') ||
+       (cellThree.textContent === 'X' && cellFive.textContent === 'X' && cellSeven.textContent === 'X')
+    ) {
+        if (showAlert) {
+            alert('Player X Won!')
+            resetGame()
+            }
+            return true
+        }
+        return false
     }
 
+function checkWinO(showAlert = true) {
+    if (
+       (cellOne.textContent === 'O' && cellFour.textContent === 'O' && cellSeven.textContent === 'O') ||
+       (cellOne.textContent === 'O' && cellTwo.textContent === 'O' && cellThree.textContent === 'O') ||
+       (cellFour.textContent === 'O' && cellFive.textContent === 'O' && cellSix.textContent === 'O') ||
+       (cellSeven.textContent === 'O' && cellEight.textContent === 'O' && cellNine.textContent === 'O') ||
+       (cellThree.textContent === 'O' && cellSix.textContent === 'O' && cellNine.textContent === 'O') ||
+       (cellTwo.textContent === 'O' && cellFive.textContent === 'O' && cellEight.textContent === 'O') ||
+       (cellOne.textContent === 'O' && cellFive.textContent === 'O' && cellNine.textContent === 'O') ||
+       (cellThree.textContent === 'O' && cellFive.textContent === 'O' && cellSeven.textContent === 'O')
+    ) {
+        if (showAlert) {
+        alert('Player O Won!')
+        resetGame()
+        }
+        return true
+    }
+    return false
 }
 
+
 function checkDraw() {
-    if ( (cellOne.textContent.includes('X') || cellOne.textContent.includes('O')) && 
+    if ( (checkWinX(false) === false) && 
+    (checkWinO(false) === false) && 
+        (cellOne.textContent.includes('X') || cellOne.textContent.includes('O')) && 
     (cellTwo.textContent.includes('X') || cellTwo.textContent.includes('O')) && 
     (cellThree.textContent.includes('X') || cellThree.textContent.includes('O')) && 
     (cellFour.textContent.includes('X') || cellFour.textContent.includes('O')) && 
@@ -128,17 +133,21 @@ function checkDraw() {
     (cellEight.textContent.includes('X') || cellEight.textContent.includes('O')) && 
     (cellNine.textContent.includes('X') || cellNine.textContent.includes('O')) ) {
 alert('Its A Draw')
+resetGame()
+return true
     }
+   
 }
 
 const reset = document.getElementById('reset')
-const cell = document.getElementsByClassName('cell')
+reset.addEventListener('click', resetGame)
 
-reset.addEventListener('click', function() {
+    function resetGame() {
+    const cell = document.getElementsByClassName('cell')
     for (let i = 0; i < cell.length; i++) {
         cell[i].textContent = ''
     }
-})
+    }
 
 
 return {
@@ -147,7 +156,6 @@ return {
     board
 }
 })()
-
 
 
 gameBoard.renderGameBoard()
